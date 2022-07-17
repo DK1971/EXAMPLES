45fac946fd8d641ca2e8a6dc39188f45a3590eaf
Дмитрий Карманов <karmanovds@gmail.com>
2022-07-17 16:11:39 +0700

    Homework for the lesson 2

---
+++ Seminar_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void Example_10()
+        {
+        //Задача 10: Напишите программу, которая принимает на вход трёхзначное число.
+        //A на выходе показывает вторую цифру этого числа.
+            Console.Write("Введите число:  ");
+			int numberX = Convert.ToInt32(Console.ReadLine());
+			if (numberX > 99 && numberX < 1000)
+            {
+                Console.WriteLine("Вторая цифра числа " + (numberX/(10%10));
+            }
+            else
+            {
+                Console.WriteLine("Введенное число не трёхзначное!");
+            }
+        }
+    
+    Example_10();
+    }
+}
