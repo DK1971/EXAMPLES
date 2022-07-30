71c8b29867da7c6d6bfe10c65a441ff1a049d8f8
Дмитрий Карманов <karmanovds@gmail.com>
2022-07-30 16:55:20 +0700

    Example_19 Paliondrom

---
+++ Seminar_03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{			
+			void Example_19()
+			{
+			//Задача 19: Напишите программу, которая принимает на вход пятизначное число,
+			//и проверяет, является ли оно палиондромом.
+			Console.Write("Введите пятизначное число:  ");
+			string input = Console.ReadLine();
+			char[] inputArray = input.ToCharArray();
+			Array.Reverse(inputArray);
+			string output = new string(inputArray);
+			Console.WriteLine(output);
+			if (input == output)
+			  Console.WriteLine("Число " + input + " является палиондромом");
+			  else
+			    Console.WriteLine("Число " + input + " не является палиондромом");
+			Console.ReadKey();
+			}
+			
+			void Example_23()
+			{
+			//Задача 23. Напишите программу, которая принимает на вход число (N)
+			//и выдаёт таблицу кубов чисел от 1 до N.
+			//Output: 3 -> 1, 8, 27 или 5 -> 1, 8, 27, 64, 125
+         	Console.Write("Ведите число N: ");
+          	double num = Convert.ToDouble(Console.ReadLine());
+            	Console.WriteLine("Таблица кубов от 1 до введенного числа N:");
+			for (int i = 1; i <= num; i++)
+            		Console.WriteLine(i*i*i);
+			Console.ReadLine();		
+			}
+		//Example_19();
+		//Example_23();
+		}
+	}
+}
