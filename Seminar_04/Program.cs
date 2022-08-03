b59394ff2d7a40219dd1cdc15e29345706e43fba
Дмитрий Карманов <karmanovds@gmail.com>
2022-08-03 22:42:04 +0700

    Summer of numbers

---
+++ Seminar_04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{			
+			void Example_24()
+			{
+				//Задача 24: Напишите программу, которая принимает на вход число (А)
+                //и выдаёт сумму чисел от 1 до А.
+
+                Console.Write("Введите число: ");
+                int numberA = Convert.ToInt32(Console.ReadLine());
+                int sum = 0;
+
+                for (int i = 1; i <= numberA; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"Сумма чисел от 1 до {numberA} равна {sum}");
+			}
+			Example_24();		
+		}
+	}
+}
+
