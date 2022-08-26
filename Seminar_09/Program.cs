0f91c3aee5117f4cab93a0b2aa808d1fdb7cc985
DK1971 <107458709+DK1971@users.noreply.github.com>
2022-08-26 21:54:35 +0700

    exercises for seminar 9

--- Seminar_09/Program.cs
+++ Seminar_09/Program.cs
@@ -10,16 +10,16 @@ namespace Exercise
  			void Exercise_64()
 			{
 			// Задача 64: Задайте значения M и N. Напишите рекурсионный метод, 
-            // который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
-            // Output: M = 1, N = 9 -> "3, 6, 9"; M = 13, N = 20 -> "15, 18" 
+            		// который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+            		// Output: M = 1, N = 9 -> "3, 6, 9"; M = 13, N = 20 -> "15, 18" 
             		 
 				Console.Clear();
 				int m = InputNumbers("Введите целое число M: ");
 				int n = InputNumbers("Введите целое число N: ");
 								
 				if (m < n)
-            	{
-                for (int i = m; i <= n; i++)
+				{
+				for (int i = m; i <= n; i++)
 					if(i %3 == 0)					
 				Console.Write(i + "\t");
 				}
@@ -34,11 +34,11 @@ namespace Exercise
 
 			}	
 
-            void Exercise_66()
+            		void Exercise_66()
 			{
 			// Задача 66: Задайте значения M и N. Напишите рекурсионный метод, 
-            // который найдёт сумму натуральных элементов в промежутке от M до N.
-            // Output: M = 1; N = 15 -> 120; M = 4; N = 8 -> 30	
+			// который найдёт сумму натуральных элементов в промежутке от M до N.
+			// Output: M = 1; N = 15 -> 120; M = 4; N = 8 -> 30	
 				
 				Console.Clear();
 				int m = InputNumbers("Введите целое число M: ");
@@ -71,38 +71,38 @@ namespace Exercise
 				return output;
 				}
 
-            }
+			}
 
-            void Exercise_68()
+			void Exercise_68()
 			{
 			// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
-            // Даны два неотрицательных числа M и N.
-            // n = 2, m = 3 -> A(m,n) = 9
-            
-                Console.Write("Введите число n: ");
-                int n = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите число m: ");
-                int m = Convert.ToInt32(Console.ReadLine());
-                int a;
-                a = ackermannFunc(n, m);
-                Console.WriteLine("Значение функции Аккермана (m,n): " + a);
-                
-                static int ackermannFunc(int n, int m)
-                {
-                    if (n == 0)
-                        return m + 1;
-                    else if (n != 0 && m == 0)
-                        return ackermannFunc(n - 1, 1);
-                    else if (n > 0 && m > 0)
-                        return ackermannFunc(n - 1, ackermannFunc(n, m - 1));
-                        return ackermannFunc(n,m);
-                }
-                 
-            }
+			// Даны два неотрицательных числа M и N.
+			// n = 2, m = 3 -> A(m,n) = 9
+
+				Console.Write("Введите число n: ");
+				int n = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Введите число m: ");
+				int m = Convert.ToInt32(Console.ReadLine());
+				int a;
+				a = ackermannFunc(n, m);
+				Console.WriteLine("Значение функции Аккермана (m,n): " + a);
+
+				static int ackermannFunc(int n, int m)
+				{
+				if (n == 0) 
+					return m + 1;
+				else if (n != 0 && m == 0)
+					return ackermannFunc(n - 1, 1);
+				else if (n > 0 && m > 0)
+					return ackermannFunc(n - 1, ackermannFunc(n, m - 1));
+					return ackermannFunc(n,m);
+				}
+
+			}
 
 			Exercise_64();
 			//Exercise_66();
-            //Exercise_68();
+            		//Exercise_68();
 
 			}
 			static void FillArray (int[,]array, int startN = 0, int finishN = 10)
